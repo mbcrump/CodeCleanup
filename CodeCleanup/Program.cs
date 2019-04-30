48eae61337f82e16d4d7a457c8a8aa29df4cb4b1
Michael Crump <mbcrump29@gmail.com>
2019-04-30 11:02:24 -0700

    Add project files.

---
+++ CodeCleanup/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System;
+using System.Linq;
+
+
+namespace CodeCleanup
+{
+    class Program
+    {
+        int myAge = 20;
+        static void Main(string[] args)
+        {
+            // THIS WRITEs a file with my name
+             Console.WriteLine("Hello, World");
+                                                Console.WriteLine("I'm a gud coder!");
+            var MyName = "Hello, World";
+
+                MyName = "Michael";
+            Console.WriteLine(MyName);
+            // WRITES A FILE NAMED tem.txzt to local HDD along with my Name
+                using (System.IO.File.Create(@"C:\Temp.txt")) File.WriteAllText("path", String.Empty);
+            if (true)
+            { Console.WriteLine("added name" + (string)MyName);
+            }
+            //todo add ability to store agE
+            
+        }
+    }
+}
---
+++ CodeCleanup/README.txt
@@ -0,0 +1,11 @@
+﻿Throughout I added funny comments and 
+Kept the default line when creating .NET Core Console
+
+
+Line 1-3 - Shows Sort Usings and Remove unnessary usings
+Line 10 - Add proper accessibility modifiers
+Line 15 - Spacing not correct
+Line 16 - replaces var with string for explicit types
+Line 21 - Doesn't allow 1 liners for usings and cleans up System.IO
+Line 22-24 - Cleans up spacing
+Line 23 - removes unnecessary casts
